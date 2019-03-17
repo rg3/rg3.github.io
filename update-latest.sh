fbe09197baab14b9edc2d6ef6c5b84878e406f34
Ricardo Garcia <commits@rg3.name>
2019-03-17 21:27:04 +0100

    Update releases.atom and versions.json in addition to LATEST_VERSION

--- update-latest.sh
+++ update-latest.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 set -e
 cd "$( dirname "$0" )"
-RESOURCE_PATH=youtube-dl/update/LATEST_VERSION
-CURRENT="$( cat "$RESOURCE_PATH" )"
-LATEST="$( curl -s "https://ytdl-org.github.io/$RESOURCE_PATH" )"
+RESOURCE_DIR=youtube-dl/update
+FLAG_RESOURCE="$RESOURCE_DIR/LATEST_VERSION"
+EXTRA_RESOURCES=("$RESOURCE_DIR/releases.atom" "$RESOURCE_DIR/versions.json")
+BASE_URL=https://ytdl-org.github.io/
+CURRENT="$( cat "$FLAG_RESOURCE" )"
+LATEST="$( curl -s "$BASE_URL$FLAG_RESOURCE" )"
 echo "Current: $CURRENT"
 echo "Latest:  $LATEST"
 if [[ "$CURRENT" != "$LATEST" ]]; then
-    echo "Updating $RESOURCE_PATH"
-    printf "%s" "$LATEST" >"$RESOURCE_PATH"
-    git add "$RESOURCE_PATH"
-    git commit -q -m "Update $RESOURCE_PATH"
+    echo "Updating $FLAG_RESOURCE"
+    printf "%s" "$LATEST" >"$FLAG_RESOURCE"
+    for f in "${EXTRA_RESOURCES[@]}"; do
+        curl -s "$BASE_URL$f" >"$f"
+    done
+    git add "$FLAG_RESOURCE" "${EXTRA_RESOURCES[@]}"
+    git commit -q -m "Updating resources to $LATEST"
     git push
 else
     echo "Versions match. Nothing to do."
